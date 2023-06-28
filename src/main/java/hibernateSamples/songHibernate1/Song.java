184814e7867a2c4516981c67a22a304ae73d2e8e
Keyur <keyur.patel0921@gmail.com>
2023-06-28 00:24:10 -0400

    version 1.1

---
+++ src/main/java/hibernateSamples/songHibernate1/Song.java
@@ -0,0 +1,40 @@
+package hibernateSamples.songHibernate1;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Song {
+	@Id
+	private int id;
+	private String songName;
+	
+	@Column(name = "singer")
+	private String artist;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getSongName() {
+		return songName;
+	}
+
+	public void setSongName(String songName) {
+		this.songName = songName;
+	}
+
+	public String getArtist() {
+		return artist;
+	}
+
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+
+}
